580eebd9287fb7c547520b3e2863a3ce599e8207
Mauro Werder <mauro3@runbox.com>
2022-10-25 14:46:02 +0200

    inline reference test

--- scripts/car_travel.jl
+++ scripts/car_travel.jl
@@ -30,3 +30,12 @@ function car_travel_1D()
 end
 
 T, X = car_travel_1D()
+
+# a reference test:
+# make inds with `sort(rand(1:length(X), 10)
+inds = [10, 28, 36, 37, 39, 69, 80, 96, 128, 135]
+# make vals with `X[inds]`
+vals = [101.69999999999999, 101.7, 11.300000000000018, 1.7763568394002505e-14, 1.7763568394002505e-14, 67.80000000000001, 33.90000000000002, 192.10000000000005, 146.9, 180.80000000000004]
+
+using Test
+@test all(vals .≈ X[inds])
